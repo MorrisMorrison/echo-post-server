825a3be3c660444c7a1ed1e1e0e61fc2c6d289b4
Robert Mang <Robert.Mang@softgarden.de>
2023-06-18 00:36:47 +0200

    adds xml description to collection extensions

--- src/Application/Common/Extensions/CollectionExtensions.cs
+++ src/Application/Common/Extensions/CollectionExtensions.cs
@@ -8,6 +8,15 @@ namespace EchoPost.Application.Common.Extensions;
 
 public static class CollectionExtensions
 {
+
+    /// <summary>
+    /// This method allows the usage of functional style for each loops on all IEnumerable
+    /// <example>
+    /// <code>
+    ///  myEnumerable.ForEach(item => Console.WriteLine(item));
+    /// </code>
+    /// </example>
+    /// </summary>
     public static void ForEach<T>(this IEnumerable<T> p_collection, Action<T> p_func)
     {
         foreach (var p_var in p_collection)
@@ -16,6 +25,14 @@ public static class CollectionExtensions
         }
     }
 
+    /// <summary>
+    /// This method allows the usage of functional style for each loops with index on all IEnumerable
+    /// <example>
+    /// <code>
+    ///  myEnumerable.ForEach((item, index) => Console.WriteLine($"Item at index {index}: {item}"));
+    /// </code>
+    /// </example>
+    /// </summary>
     public static void ForEach<T>(this IEnumerable<T> p_collection, Action<T, int> p_func)
     {
         for (int i = 0; i < p_collection.Count(); i++)
