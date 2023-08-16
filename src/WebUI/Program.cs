d3238ed1fd40efd6ba69eca0abec0a8ee89b9d4c
Robert Mang <Robert.Mang@softgarden.de>
2023-08-17 00:43:23 +0200

    disable https

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -10,25 +10,16 @@ builder.Services.AddWebUIServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseMigrationsEndPoint();
-
-    // Initialise and seed database
-    using var scope = app.Services.CreateScope();
-    var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
-    await initialiser.InitialiseAsync();
-    await initialiser.SeedAsync();
-}
-else
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+// Initialise and seed database
+using var scope = app.Services.CreateScope();
+var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+await initialiser.InitialiseAsync();
+await initialiser.SeedAsync();
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+// app.UseHsts();
 
 app.UseHealthChecks("/health");
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 // app.UseStaticFiles();
 
 app.UseSwaggerUi3(settings =>
