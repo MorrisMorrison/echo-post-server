4ca70cc3995f8e50411cc84746a27dc88142132b
Robert Mang <Robert.Mang@softgarden.de>
2023-08-23 02:36:17 +0200

    improve console logging

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,22 +1,31 @@
 using EchoPost.Infrastructure.Persistence;
 using Microsoft.AspNetCore.CookiePolicy;
 
-
+Console.WriteLine("--- STARTING ECHOPOST WITH ---");
+Console.WriteLine(args);
+Console.WriteLine("--- BUILD APPLICATION ---");
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+Console.WriteLine("--- SETUP APPLICATION SERVICES ---");
 builder.Services.AddApplicationServices();
+Console.WriteLine("--- SETUP INFRASTRUCTURE SERVICES ---");
 builder.Services.AddInfrastructureServices(builder.Configuration);
+Console.WriteLine("--- SETUP WEBUI SERVICES ---");
 builder.Services.AddWebUIServices();
+Console.WriteLine("--- SET ENVIRONMENT VARIABLES ---");
 builder.Configuration.AddEnvironmentVariables();
+Console.WriteLine();
+Console.WriteLine("--- WITH CONFIGURATION ---");
 
 string connectionString = builder.Configuration.GetConnectionString("AZURE_MYSQL_CONNECTIONSTRING");
 string env = builder.Configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT");
 string urls = builder.Configuration.GetValue<string>("ASPNETCORE_URLS");
 
-Console.WriteLine("--- STARTING APPLICATION WITH ---");
 Console.WriteLine("AZURE_MYSQL_CONNECTIONSTRING:" + connectionString);
 Console.WriteLine("ASPNETCORE_ENVIRONMENT:" + env);
 Console.WriteLine("ASPNETCORE_URLS:" + urls);
+Console.WriteLine();
+
 
 builder.Services.AddCors(options => options.AddPolicy(name: "echopost-angular-client", policy => policy.WithOrigins("https://echopost-angular-client.azurewebsites.net", "http://localhost:44447")
 .AllowAnyHeader()
@@ -44,6 +53,8 @@ app.UseCookiePolicy(new CookiePolicyOptions
     MinimumSameSitePolicy = SameSiteMode.None,
     Secure = CookieSecurePolicy.Always
 });
+
+Console.WriteLine("--- SETUP IDENTITY ---");
 app.UseAuthentication();
 app.UseIdentityServer();
 app.UseAuthorization();
@@ -53,5 +64,6 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 app.UseStaticFiles();
+Console.WriteLine("--- FINISHED ---");
 
 app.Run();
